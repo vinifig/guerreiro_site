29bc9a909d67f0c63d75f34030058b25b935395b
Carolina Junqueira <junqueiracarolina@live.com>
2016-06-09 02:53:56 -0300

    pagina cadastro-funcionario estruturada

---
+++ modulos/gerencia/cadastroFuncionario.php
@@ -0,0 +1,27 @@
+<?php
+  $title_page = "Cadastrar Funcionário";
+  // $menu_items = [
+  //   ["nome" => "oi", "url" => "#1"],
+  //   ["nome" => "cueio", "url" => "#2"],
+  // ];
+  include 'header.php';
+ ?>
+ 	  <div id="inputFields" class="list">
+			<label class="item">
+				Nome
+				<input type="text" placeholder="Joao Ferreira Silva">
+			</label>
+			<label class="item">
+				CPF
+				<input type="text" placeholder="ex: 12345678910">
+			</label>
+			<label class="item">
+				Senha
+				<input type="password" placeholder="sua senha aqui">
+			</label>
+		</div>
+		<button class="button button-positive">Cadastrar</button>
+
+<?php
+  include 'footer.php';
+?>
