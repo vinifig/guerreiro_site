777c89c056b7bceb920d62d9fcee7b79aaf837e4
Carolina Junqueira <junqueiracarolina@live.com>
2016-06-09 18:35:22 -0300

    pagina pedidos consertada

--- modulos/comuns/pedidos.php
+++ modulos/comuns/pedidos.php
@@ -3,20 +3,23 @@
 
   include '../../header.php';
  ?>
-		<div class="list">
+		<div class="card list">
 			<a class="item item-thumbnail-left" href="#">
-	      <img src="//<?php echo $url_server; ?>img/hamburger.png">
-	      <h2>Hamburger</h2>
-	      <p>Sanduíche delicioso.</p>
-				<p>Ovo, Tomate, Alface, Hamburger</p>
+	      <img src="//<?php echo $url_server; ?>img/conta.png">
+				<h2>#123</h2>
+				<p>Data Entrega: 01/01/16 Hora Entrega: 08:40 PREÇO: R$ 69,90 Data Pedido: 01/01/16 Hora Pedido: 08:40</p>
 	    </a>
 			<a class="item item-thumbnail-left" href="#">
-	      <img src="//<?php echo $url_server; ?>img/hamburger.png">
-	      <h2>Hamburger</h2>
-	      <p>Sanduíche delicioso.</p>
-				<p>Ovo, Tomate, Alface, Hamburger</p>
+	      <img src="//<?php echo $url_server; ?>img/conta.png">
+				<h2>#123</h2>
+				<p>Data Entrega: 01/01/16 Hora Entrega: 08:40 PREÇO: R$ 69,90 Data Pedido: 01/01/16 Hora Pedido: 08:40</p>
 	    </a>
-		</div>
+			<a class="item item-thumbnail-left" href="#">
+	      <img src="//<?php echo $url_server; ?>img/conta.png">
+				<h2>#123</h2>
+				<p>Data Entrega: 01/01/16 Hora Entrega: 08:40 PREÇO: R$ 69,90 Data Pedido: 01/01/16 Hora Pedido: 08:40</p>
+	    </a>
+	  </div>
   <?php
   include '../../footer.php';
 ?>
