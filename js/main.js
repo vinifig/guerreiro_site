8378a04f3bde17cff4482484dd8032b46b5cd3bd
vinifig <vinifig@hotmail.com>
2016-06-10 02:38:13 -0300

    If verification

--- js/main.js
+++ js/main.js
@@ -1,4 +1,32 @@
+// DEFAULT FUNCTIONS
 var changeTitleBar = function(title){
   if(title)
   $("#title_bar").text(title);
 }
+
+// LOGIN
+
+var loginSubmitCallback = function(e){
+  var loginInfo = $(this).serializeArray();
+
+  var loginCallback = function(data){
+    console.log(data)
+  }
+
+  if(loginInfo[0].value == "cliente"){
+    cliente.auth({
+      num_celular : loginInfo[1].value,
+      senha : loginInfo[2].value
+    }, loginCallback)
+  }else{
+    funcionario.auth({
+      cpf_funcionario : loginInfo[1].value,
+      senha : loginInfo[2].value
+    }, loginCallback)
+  }
+  e.preventDefault();
+}
+
+$(document).ready(function() {
+  $("#loginForm").on("submit", loginSubmitCallback)
+});
--- login.php
+++ login.php
@@ -6,20 +6,23 @@
   // ];
   include 'header.php';
  ?>
+  <form id="loginForm">
  	 <div id="loginRadioChoice">
 		 <input type="radio" checked name="tipoLogin" value="cliente">Cliente</input>
 		 <input type="radio" name="tipoLogin" value"funcionario">Funcionario</input>
 	 </div>
 	 <div class="list">
 	   <label class="item item-input item-floating-label">
-	   <input type="text" placeholder="Telefone">
+	   <input name="login" type="text" placeholder="login">
 	   </label>
 	   <label class="item item-input item-floating-label">
-	     <input type="password" placeholder="Senha">
+	     <input name="senha" type="password" placeholder="Senha">
 	   </label>
-	   <a class="button button-positive">Login</a>
-	   <a class="button button-positive">Sign up</a>
+	   <input type="submit" class="button button-positive" value="Login">
+	   <a href="//<?php echo $url_server; ?>cadastroCliente.php" class="button button-positive">Sign up</a>
 	 </div>
+ </form>
+
 <?php
   include 'footer.php';
 ?>
