92895804d8db592765767fa0f3279daa334dadaf
Carolina Junqueira <junqueiracarolina@live.com>
2016-06-11 03:37:22 -0300

    pagina lista de produtos (gerencia) pronta

---
+++ modulos/gerencia/listaProdutos.php
@@ -0,0 +1,30 @@
+<?php
+  $title_page = "Produtos";
+
+  include '../../header.php';
+ ?>
+ 		 <button class="button button-positive">Adicionar Produto</button>
+		 <div class="card list">
+		 	<a class="item item-thumbnail-left" href="#">
+		 		<img src="//<?php echo $url_server; ?>img/hamburger.png">
+		 		<h2>Hamburger</h2>
+				<p>Delicioso sanduíche.</p>
+		 		<p>Ovo, Tomate, Hamburger</p>
+		 	</a>
+			<a class="item item-thumbnail-left" href="#">
+		 		<img src="//<?php echo $url_server; ?>img/hamburger.png">
+		 		<h2>Hamburger</h2>
+				<p>Delicioso sanduíche.</p>
+		 		<p>Ovo, Tomate, Hamburger</p>
+		 	</a>
+			<a class="item item-thumbnail-left" href="#">
+		 		<img src="//<?php echo $url_server; ?>img/hamburger.png">
+		 		<h2>Hamburger</h2>
+				<p>Delicioso sanduíche.</p>
+		 		<p>Ovo, Tomate, Hamburger</p>
+		 	</a>
+
+		 </div>
+	<?php
+  include '../../footer.php';
+?>
