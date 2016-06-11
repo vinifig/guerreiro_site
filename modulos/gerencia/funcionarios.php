04d99c01689a8cd596e4ab6cf543e7703ada47dd
Carolina Junqueira <junqueiracarolina@live.com>
2016-06-11 02:45:31 -0300

    pagina funcionarios pronta

---
+++ modulos/gerencia/funcionarios.php
@@ -0,0 +1,31 @@
+<?php
+  $title_page = "Funcionários";
+  // $menu_items = [
+  //   ["nome" => "oi", "url" => "#1"],
+  //   ["nome" => "cueio", "url" => "#2"],
+  // ];
+  include '../../header.php';
+ ?>
+ 	  <div id="listaFuncionario" class="list">
+				<a class="card item item-avatar">
+					<img src="//<?php echo $url_server; ?>img/empregado.png" href="#">
+					<h2>Ana Clara Braga da Motta</h2>
+					<p>123.456.789-00</p>
+					<button class="button icon ion-close-round"></button>
+				</a>
+				<a class="card item item-avatar">
+					<img src="//<?php echo $url_server; ?>img/empregado.png" href="#">
+					<h2>Ana Clara Braga da Motta</h2>
+					<p>123.456.789-00</p>
+					<button class="button icon ion-close-round"></button>
+				</a>
+				<a class="card item item-avatar">
+					<img src="//<?php echo $url_server; ?>img/empregado.png" href="#">
+					<h2>Ana Clara Braga da Motta</h2>
+					<p>123.456.789-00</p>
+					<button class="button icon ion-close-round"></button>
+				</a>
+		</div>
+<?php
+  include '../../footer.php';
+?>
